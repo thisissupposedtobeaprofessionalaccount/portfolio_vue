7c95eddf7f0409bc5b712b1780e97806c17a435b
hm <hugo.marin.adressepro@gmail.com>
2025-04-06 17:49:54 +0200

    scroll to top on projects

--- src/main.js
+++ src/main.js
@@ -17,6 +17,9 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+   scrollBehavior(to, from, savedPosition) {
+    return { top: 0 }
+  }
 })
 
 router.afterEach((to) => {
